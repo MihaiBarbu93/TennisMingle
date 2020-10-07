50cfb212e758d63f1d73a9d2bc299e02f1401a1d
Barbu Mihai <barbu.mihai1993@gmail.com>
2020-10-07 16:06:19 +0300

    Actions added for Tennis Club

---
+++ TennisMingle.API/Models/TennisClubForUpdateDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using TennisMingle.API.Enums;
+
+namespace TennisMingle.API.Models
+{
+    public class TennisClubForUpdateDTO
+    {
+        [Required(ErrorMessage ="You should provide a name value")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public ICollection<TennisCourtDTO> TennisCourts { get; set; }
+
+        public ICollection<CoachDTO> Coaches { get; set; }
+
+        public ICollection<Surface> Surfaces { get; set; }
+
+        public ICollection<Facilities> Facilities { get; set; }
+
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Description { get; set; }
+        public List<int> Prices { get; set; }
+        public string Schedule { get; set; }
+        public string Image { get; set; }
+    }
+}
