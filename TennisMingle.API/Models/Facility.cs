202437920207f3aaf84903319f625b01690538d9
Barbu Mihai <barbu.mihai1993@gmail.com>
2020-10-20 16:57:54 +0300

    Create seeder for database

--- TennisMingle.API/Models/Facility.cs
+++ TennisMingle.API/Models/Facility.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using TennisMingle.API.Enums;
@@ -12,6 +13,8 @@ namespace TennisMingle.API.Models
         public int Id { get; set; }
         [Required]
         public Facilities FacilityType { get; set; }
+        public int TennisClubId { get; set; }
+        [ForeignKey("TennisClubId")]
         public TennisClubDTO TennisClub { get; set; }
     }
 }
