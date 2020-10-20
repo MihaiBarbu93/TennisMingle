3e217ec874e8da7b44cf8f77fdb511066dad0b0a
silviurdr <silviu.catrina@gmail.com>
2020-10-20 11:27:38 +0300

    Setup startup configuration for database

--- TennisMingle.API/Models/AppDbContext.cs
+++ TennisMingle.API/Models/AppDbContext.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace TennisMingle.API.Models
 {
-    public class AppDbContext
+    public class AppDbContext: DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CityDTO> Cities { get; set; }
+        public DbSet<TennisClubDTO> TennisClubs { get; set; }
+        public DbSet<TennisCourtDTO> TennisCourts { get; set; }
+        public DbSet<PersonDTO> Person { get; set; }
+
+
     }
-}
