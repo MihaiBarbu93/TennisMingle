13803644ef41d51705e10360c415ff9c1306d812
Barbu Mihai <barbu.mihai1993@gmail.com>
2020-10-20 14:48:28 +0300

    Make some properties optionals

---
+++ TennisMingle.API/Migrations/20201020114632_CoachForBooking.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TennisMingle.API.Migrations
+{
+    public partial class CoachForBooking : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TennisClubId",
+                table: "Persons",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PersonId",
+                table: "Bookings",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Bookings",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Bookings");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TennisClubId",
+                table: "Persons",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PersonId",
+                table: "Bookings",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+    }
+}
