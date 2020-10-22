c9faf8a58acf282823ac1c6ff37df50022348db5
Barbu Mihai <barbu.mihai1993@gmail.com>
2020-10-22 13:50:54 +0300

    Add header image for index page

---
+++ TennisMingle.WEB/wwwroot/NewFolder/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TennisMingle.WEB.wwwroot.NewFolder
+{
+    public class Class
+    {
+    }
+}
