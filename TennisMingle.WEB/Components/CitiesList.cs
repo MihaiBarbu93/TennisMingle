f0fd914c38ee10cd3f8ae15e01746851a1a4c6fc
Silviu <Silviu@DESKTOP-MOAPUM6>
2020-10-22 12:37:40 +0300

    Add TennisMingle.WEB start project

---
+++ TennisMingle.WEB/Components/CitiesList.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TennisMingle.API.Models;
+
+namespace TennisMingle.WEB.Components
+{
+    public class CitiesList: ViewComponent
+    {
+        private readonly AppDbContext _context;
+        public CitiesList(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var cities = _context.Cities.OrderBy(c => c.Name);
+            return View(cities);
+        }
+    }
+}
---
+++ TennisMingle.WEB/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using TennisMingle.API.Models;
+using TennisMingle.WEB.Models;
+
+namespace TennisMingle.WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<CityDTO> cities = new List<CityDTO>();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:44313/api/cities"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    cities = JsonConvert.DeserializeObject<List<CityDTO>>(apiResponse);
+                }
+            }
+
+            return View(cities);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
