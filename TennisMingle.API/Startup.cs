33347511eaa7f7f0ee0bf9d13e185322f3383d0d
Silviu <silviu.catrina@gmail.com>
2020-12-09 13:00:42 +0200

    Configure HTTPS

--- TennisMingle.API/Startup.cs
+++ TennisMingle.API/Startup.cs
@@ -54,7 +54,7 @@ namespace TennisMingle.API
             app.UseCors(x => x.AllowAnyHeader()
                 .AllowAnyMethod()
                 .AllowCredentials()
-                .WithOrigins("http://localhost:4200"));
+                .WithOrigins("https://localhost:4200"));
 
             app.UseAuthentication();
             app.UseAuthorization();
