f8b7eb2769304d8c8c935c7e3f723e54c058919c
Silviu <silviu.catrina@gmail.com>
2020-12-10 04:14:30 +0200

    Configure fontawesome

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,12 +8,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { TennisClubListComponent } from './tennis-club/tennis-club-list/tennis-club-list.component';
 import { TennisClubDetailComponent } from './tennis-club/tennis-club-detail/tennis-club-detail.component';
 import { TennisClubCardComponent } from './tennis-club/tennis-club-card/tennis-club-card.component';
 import { FooterComponent } from './footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -32,6 +32,7 @@ import { FooterComponent } from './footer/footer.component';
     BrowserAnimationsModule,
     FormsModule,
     SharedModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
