b2a9e79703ba59d891b2767baeb7a2d49fa8fa57
Barbu Mihai <barbu.mihai1993@gmail.com>
2020-12-15 15:27:40 +0200

    Fix users table

---
+++ TennisMingle.API/Migrations/20201215124006_CityNullableForUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TennisMingle.API.Migrations
+{
+    public partial class CityNullableForUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CityId",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CityId",
+                table: "Users",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+    }
+}
