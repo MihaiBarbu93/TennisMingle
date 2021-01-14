941916525095bc1b99c8680f6fb8dfca81be064b
Barbu Mihai <barbu.mihai1993@gmail.com>
2021-01-14 20:09:15 +0200

    Validating login fields

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 
 @Component({
@@ -12,12 +14,28 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   @Input() modalRefFromNavComponent: any;
   model: any = {};
+  loginForm =new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
 
   constructor(public accountService: AccountService, 
     private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      username: new FormControl(this.model.username, [
+        Validators.required
+  
+      ]),
+      password: new FormControl(this.model.password,[
+        Validators.required
+      ])
+
+    });
   }
+  get username() { return this.loginForm.get('username'); }
+  get password() { return this.loginForm.get('password'); }
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
