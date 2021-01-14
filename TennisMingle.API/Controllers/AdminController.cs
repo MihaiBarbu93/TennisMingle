17795450e0d3628e30728dbdad7f090e9b4511ff
Silviu <silviu.catrina@gmail.com>
2021-01-14 23:32:11 +0200

    Add get action to receive all the users with their roles

--- TennisMingle.API/Controllers/AdminController.cs
+++ TennisMingle.API/Controllers/AdminController.cs
@@ -1,20 +1,41 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TennisMingle.API.Entities;
 
 namespace TennisMingle.API.Controllers
 {
     public class AdminController: BaseApiController
     {
+        private readonly UserManager<AppUser> _userManager;
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
 
         [Authorize(Policy="RequireAdminRole")]
         [HttpGet("users-with-roles")]
-        public ActionResult GetUsersWithRoles()
+        public async Task<ActionResult> GetUsersWithRoles()
         {
-            return Ok("Only admins can see this");
+            var users = await _userManager.Users
+                .Include(r => r.UserRoles)
+                .ThenInclude(r => r.Role)
+                .OrderBy(u => u.UserName)
+                .Select(u => new
+                {
+                    u.Id,
+                    Username = u.UserName,
+                    Roles = u.UserRoles.Select(r => r.Role.Name).ToList()
+                })
+                .ToListAsync();
+
+            return Ok(users);
         }
 
         [Authorize(Policy = "RequireCoachRole")]
