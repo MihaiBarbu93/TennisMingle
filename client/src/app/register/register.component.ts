b51d06ab289b0317f5b0a6725e47fa1cb240b86c
Barbu Mihai <barbu.mihai1993@gmail.com>
2021-01-14 20:09:15 +0200

    Validating login fields

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -22,7 +22,7 @@ export class RegisterComponent implements OnInit {
     displayKey:"description", //if objects array passed which key to be displayed defaults to description
     search:true, //true/false for the search functionlity defaults to false,
     height: 'auto', //height of the list so that if there are more no of items it can show a scroll defaults to auto. With auto height scroll will never appear
-    placeholder:'Select your type', // text to be displayed when no item is selected defaults to Select,
+    placeholder:'Select your role', // text to be displayed when no item is selected defaults to Select,
     customComparator: ()=>{}, // a custom function using which user wants to sort the items. default is undefined and Array.sort() will be used in that case,
     limitTo: 0 ,// number thats limits the no of options displayed in the UI (if zero, options will not be limited)
     moreText: 'more', // text to be displayed whenmore than one items are selected like Option 1 + 5 more
